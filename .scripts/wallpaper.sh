9e4d184338f7714765281d19fdd779d44dc51774
ScriptGo <yebuyu@gmail.com>
2024-07-27 10:10:26 +0800

    重构代码

--- .scripts/wallpaper.sh
+++ .scripts/wallpaper.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 swww-daemon
+
+DIR="$HOME/Pictures/wallpaper"
+
 while true; do
-    DIR=$HOME/Pictures/wallpaper
-    PICS=($(ls ${DIR}))
-    RANDOMPICS=${PICS[$RANDOM % ${#PICS[@]}]}
-    swww img ${DIR}/"${RANDOMPICS}" --transition-fps 30 --transition-type random --transition-duration 3
+    PICS=("$DIR"/*)
+    RANDOMPIC="${PICS[RANDOM % ${#PICS[@]}]}"
+    swww img "$RANDOMPIC" --transition-fps 30 --transition-type random --transition-duration 3
     sleep 60
 done
